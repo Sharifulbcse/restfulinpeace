45835dab49f67b628569fbec85a5b27c15dc96d9
MDSI <sharifulbcse@gmail.com>
2019-01-17 19:24:30 +0600

    UTC time code in RestfulinpeaceApplication.java

--- src/main/java/com/example/restfulinpeace/RestfulinpeaceApplication.java
+++ src/main/java/com/example/restfulinpeace/RestfulinpeaceApplication.java
@@ -2,13 +2,27 @@ package com.example.restfulinpeace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+        RestfulinpeaceApplication.class,
+        Jsr310JpaConverters.class
+})
 public class RestfulinpeaceApplication {
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(RestfulinpeaceApplication.class, args);
-        System.out.println("Hello world");
+        //System.out.println("Hello world");
     }
 
 }
